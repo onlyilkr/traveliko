ac841979ef40908d0f9d209769cef2908d094fd7
İlker Güldalı <teknolojik@yandex.com>
2021-05-20 23:03:18 +0300

    google analytics package added

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import VueMeta from 'vue-meta'
+import VueAnalytics from 'vue-analytics';
+
+Vue.use(VueAnalytics, {
+    id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+    router
+});
 
 if (process.env.NODE_ENV === "production") {
   console.debug = function () {};
